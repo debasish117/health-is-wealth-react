ae0cc555f7f9d784c049a4fb08c53b2349ab4007
debasish <debasish.dhal117@gmail.com>
2024-07-27 13:35:41 +0530

    router and material ui setups done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Login from './pages/Login';
+import ResponsiveAppBar from './components/NavBar';
+
+function App() {
+
+  return (
+    <div>
+      <ResponsiveAppBar />
+      <Login />
+    </div>
+  )
+}
+
+export default App
